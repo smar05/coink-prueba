ac37ed4cdb965e0f48e7583f10431be01d918eb2
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-29 00:04:26 -0500

    Pantalla splash

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomePage {
-
   constructor() {}
-
 }
