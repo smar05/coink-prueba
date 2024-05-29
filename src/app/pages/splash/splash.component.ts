be3c88df16569dc0ae64ad6ae9a9f70f97f0c0d4
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-29 00:22:48 -0500

    Se pasa la pantalla de splash a un componente

---
+++ src/app/pages/splash/splash.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SplashComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
