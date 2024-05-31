89add027bbd38d2d1ecb1508a0d5c1e573af738a
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-05-30 23:22:17 -0500

    Pantalla de registro exitoso

--- src/app/pages/registro-exitoso/registro-exitoso.component.ts
+++ src/app/pages/registro-exitoso/registro-exitoso.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-registro-exitoso',
   templateUrl: './registro-exitoso.component.html',
   styleUrls: ['./registro-exitoso.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class RegistroExitosoComponent  implements OnInit {
-
-  constructor() { }
+export class RegistroExitosoComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
